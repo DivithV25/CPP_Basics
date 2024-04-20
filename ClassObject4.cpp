2352ad9a1ec20b5e7d84160d2c9a71692c6abe5e
DivithV25 <divithv25@gmail.com>
2024-04-20 19:23:12 +0530

    Inheritence

---
+++ ClassObject4.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+class Employee
+{
+public:
+    string name;
+    int empid;
+    int salary;
+
+    Employee(string n, int eid, int s, int pass)
+    {
+        this->name = n;
+        this->empid = eid;
+        this->salary = s;
+        this->password = pass;
+    }
+
+    void displaydetails()
+    {
+        cout << "Emp id:" << this->empid << endl;
+        cout << "Emp Name:" << this->name << endl;
+        cout << "Salary:" << this->salary << endl;
+    }
+    void displaypass()
+    {
+        cout << "The password is" << this->password << endl;
+    }
+
+private:
+    int password; // this key can be accessed on only in this class not in main function
+};
+// example for inheritence
+class software_engineer : public Employee
+{
+public:
+    int errors;
+};
+int main()
+{
+
+    cout << endl
+         << "Constructor" << endl;
+    cout << endl
+         << "Inheritence" << endl;
+    Employee one("Varun", 1, 100000000, 1234);
+
+    one.displaydetails();
+    one.displaypass();
+    cout << endl;
+    Employee two("Divith", 2, 20000000, 5678);
+    two.displaydetails();
+    two.displaypass();
+}
