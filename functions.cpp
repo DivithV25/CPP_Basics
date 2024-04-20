701345222e8059cf0357ec5197c38f5d16e60f0b
DivithV25 <divithv25@gmail.com>
2024-04-20 19:29:41 +0530

    Functions used to implement stack

---
+++ functions.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+#define MAX 5
+int stack[MAX];
+int top = -1;
+void display()
+{
+    int i;
+    for (i = top; i != -1; i--)
+    {
+        cout << stack[i] << endl;
+    }
+}
+void pop()
+{
+    if (top == -1)
+    {
+        cout << "stack is empty" << endl;
+        return;
+    }
+    top = top - 1;
+}
+void push(int x)
+{
+    if (top == MAX - 1)
+    {
+        cout << "overflow" << endl;
+        return;
+    }
+    top = top + 1;
+    stack[top] = x;
+}
+int main()
+{
+    int ch;
+    while (1)
+    {
+        cout << "enter the operation to be preformed" << endl;
+        cout << "1.push" << endl
+             << "2.push" << endl
+             << "3.display" << endl;
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+            int x;
+            cout << "enter the element to be pushed";
+            cin >> x;
+            push(x);
+            break;
+
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+
+        default:
+            break;
+        }
+    }
+}
