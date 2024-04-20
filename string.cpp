10dfcc0c08dae9f5e1d4ca2d544f35cabf447f92
DivithV25 <divithv25@gmail.com>
2024-04-20 18:53:55 +0530

    Strings

---
+++ string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#include<string>
+int main(){
+ string name ="Java";
+ cout<<"The name is "<<name<<endl;
+ cout<<"The length of the name is "<<name.length()<<endl;
+ cout<<"The name is "<<name.substr(0,3)<<endl;
+
+ 
+
+
+}
