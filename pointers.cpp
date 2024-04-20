6f9c6b3c0d83d7440cb0defd109a3feb3a7625c9
DivithV25 <divithv25@gmail.com>
2024-04-20 19:02:16 +0530

    Pointers

--- pointers.cpp
+++ pointers.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-int a = 40;
-int *ptr;
-ptr=&a;
-cout<<ptr<<endl;
-cout<<*ptr<<endl;
-
+int main()
+{
+    int a = 40;
+    int *ptr;
+    ptr = &a;
+    cout << ptr << endl;
+    cout << *ptr << endl;
 }
