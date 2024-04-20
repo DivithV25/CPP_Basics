541b2d2196c6d8a12b05ca33d85ef69a58df49db
DivithV25 <divithv25@gmail.com>
2024-04-20 18:39:41 +0530

    declaring variables and operations

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+int const c=87.77777;// after using const we cannot the variable will not change thrughout the program
+cout<<"enter number 1 and 2"<<endl;
+cin>>a>>b;
+cout<<a+b<<endl;
+cout<<(float)a/b<<endl;
+cout<<(float)c<<endl;
+}
