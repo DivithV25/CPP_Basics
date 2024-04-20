df6112d5026f0477c5a93f37d5b5d7eb7581adfc
DivithV25 <divithv25@gmail.com>
2024-04-20 18:52:39 +0530

    Do while loop

---
+++ dowhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+    do
+    {
+        cout << "The value of i is " << i<<endl;
+        i++;
+    } while (i > 10);
+    int j=0;
+    do{
+        cout << "The value of j is " << j<<endl;
+        j++;
+    }while(j<10);
+}
