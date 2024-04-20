ca59f233f5441ba6c264994f1a200aa82598a42b
DivithV25 <divithv25@gmail.com>
2024-04-20 19:09:06 +0530

    constructors

---
+++ ClassObject2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class Employee
+{
+public:
+    string name;
+    int empid;
+    int salary;
+
+    Employee(string n, int eid, int s)
+    {
+        this->name = n;
+        this->empid = eid;
+        this->salary = s;
+    }
+
+    void displaydetails()
+    {
+        cout << "Emp id:" << this->empid << endl;
+        cout << "Emp Name:" << this->name << endl;
+        cout << "Salary:" << this->salary << endl;
+    }
+};
+int main()
+{
+
+    cout << endl
+         << "Constructor" << endl;
+
+    Employee one("Varun", 1, 100000000);
+
+    //    one.name="Varun";
+    //    one.empid=1;
+    //    one.salary=100000000;
+    one.displaydetails();
+    cout << endl;
+    Employee two("Divith", 2, 20000000);
+    //    two.name="Divith";
+    //    two.empid=2;
+    //    two.salary=200000000;
+    two.displaydetails();
+}
